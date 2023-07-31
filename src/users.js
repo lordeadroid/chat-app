85b0e2466cfcfc574ae6212c31a6f9cc77262868
Rishabh <lordeadroid@users.noreply.github.com>
2023-07-31 16:15:56 +0530

    add feature to display unread messages

--- src/users.js
+++ src/users.js
@@ -2,16 +2,24 @@ const User = require("./user");
 
 class Users {
   #users;
+  #messages;
 
   constructor() {
     this.#users = [];
+    this.#messages = [];
   }
 
-  #broadcast(socket, name, data) {
+  #displayUnreadMessages(user) {
+    this.#messages.forEach(({ name, message }) => user.write(name, message));
+  }
+
+  #broadcast(socket, name, message) {
+    this.#messages.push({ name, message });
+
     this.#users
       .filter((user) => user.isDistinct(socket))
       .forEach((user) => {
-        user.write(name, data);
+        user.write(name, message);
       });
   }
 
@@ -19,12 +27,13 @@ class Users {
     socket.setEncoding("utf-8");
     socket.write("Enter your name : ");
 
-    socket.once("data", (name) => {
-      const user = new User(socket, name.trim());
+    socket.once("data", (data) => {
+      const user = new User(socket, data.trim());
 
       this.#users.push(user);
       user.greet();
-      user.onData((name, data) => this.#broadcast(socket, name, data));
+      this.#displayUnreadMessages(user);
+      user.onData((name, message) => this.#broadcast(socket, name, message));
     });
   }
 }
